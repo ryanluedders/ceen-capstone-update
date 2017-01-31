1beffae222ea803a681f3f2b3830d5a1704b3d63
Ryan Luedders <ryan.luedders@gmail.com>
2017-01-30 22:56:10 -0600

    finish imagesource REST service for providing images

--- imagesource/src/main/java/com/ryanluedders/capstone/ImagesourceEndpoint.java
+++ imagesource/src/main/java/com/ryanluedders/capstone/ImagesourceEndpoint.java
@@ -1,6 +1,9 @@
 package com.ryanluedders.capstone;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -8,20 +11,37 @@ import java.util.Map;
 
 import javax.annotation.PostConstruct;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ImagesourceEndpoint {
     
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public class ImageNotFoundException extends RuntimeException {
+        private static final long serialVersionUID = 1L; 
+    }
+    
     private final Log log = LogFactory.getLog(getClass());
     
     private Map<String, List<File>> files = new HashMap<>();
     
-    private Map<String, File> currentImage = new HashMap<>();
+    private Map<String, Integer> currentImage = new HashMap<>();
+    
+    @Scheduled(fixedDelayString = "${com.ryanluedders.capstone.image.duration.ms}")
+    public void periodicRefresh() {
+        for (String k : currentImage.keySet()) {
+            currentImage.put(k, ((currentImage.get(k) + 1) % files.get(k).size()));
+        }
+    }
     
     @PostConstruct
     public void initialize() {
@@ -39,20 +59,21 @@ public class ImagesourceEndpoint {
         }
         
         for (String s : files.keySet()) {
-            currentImage.put(s, files.get(s).get(0));
+            currentImage.put(s, 0);
         }
         
         log.info("loaded images: " + files.toString());
         log.info("current images: " + currentImage.toString());
-        
     }
 	
-	@RequestMapping("/parkinglots/{lot}")
-	public String currentImage(@PathVariable("lot") String lot) {
+	@RequestMapping(value = "/parkinglots/{lot}", produces = MediaType.IMAGE_JPEG_VALUE)
+	public byte[] currentImage(@PathVariable("lot") String lot) throws IOException {
 	    if (currentImage.containsKey(lot)) {
-	        return currentImage.get(lot).getName();
+	        InputStream is = new FileInputStream(files.get(lot).get(currentImage.get(lot)));
+	        return IOUtils.toByteArray(is);
 	    }
-	    return "NOT FOUND";
+	    
+	    throw new ImageNotFoundException();
 	}
 	
 	
